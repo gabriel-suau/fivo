0a777a987eb4a4434ecfc5cadddfd8493e1fe717
Gabriel Suau <gabriel.suau@cea.fr>
2024-07-11 17:42:44 +0200

    [MATH] Add functions to compute L1, L2 and Linf errors

--- include/fivo/math.h
+++ include/fivo/math.h
@@ -1,8 +1,11 @@
 #ifndef FIVO_MATH_H
 #define FIVO_MATH_H
 
+#include "state.h"
+
 #include <type_traits>
 #include <cmath>
+#include <algorithm>
 
 namespace fivo {
 
@@ -20,6 +23,43 @@ newton_raphson(T const& x0, F&& f, DF&& df, T const& tol) {
     if (dx < tol) converged = true;
   }
   return x;
+}
+
+template<typename State>
+inline auto error_l1(global_state<State> const& X,
+                     global_state<State> const& Y,
+                     double const& dx) {
+  State err = {0.};
+  for (std::size_t i = 0; i < X.size(); ++i)
+    for (std::size_t v = 0; v < X[i].size(); ++v)
+      err[v] += std::abs(X[i][v] - Y[i][v]);
+  return err * dx;
+}
+
+template<typename State>
+inline auto error_l2(global_state<State> const& X,
+                     global_state<State> const& Y,
+                     double const& dx) {
+  State err = {0.};
+  for (std::size_t i = 0; i < X.size(); ++i) {
+    auto const diff = X[i] - Y[i];
+    for (std::size_t v = 0; v < X[i].size(); ++v) {
+      err[v] += diff[v] * diff[v];
+    }
+  }
+  std::for_each(err.begin(), err.end(), [](auto& e) { e = std::sqrt(e); });
+  return err * std::sqrt(dx);
+}
+
+template<typename State>
+inline auto error_linf(global_state<State> const& X,
+                       global_state<State> const& Y,
+                       double const& = 1) {
+  State err = {0.};
+  for (std::size_t i = 0; i < X.size(); ++i)
+    for (std::size_t v = 0; v < X[i].size(); ++v)
+      err[v] = std::max(err[v], std::abs(X[i][v] - Y[i][v]));
+  return err;
 }
 
 } // namespace math
